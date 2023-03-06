089518b559473202abb4316beeeca6f5c7f184f4
Mario Ezquerro <73311502+marioEzquerro@users.noreply.github.com>
2023-03-06 09:41:39 +0100

    Añadidos metodos genericos (findBy scrollIntoView)

--- src/test/java/com/project/www/pages/AbstractPage.java
+++ src/test/java/com/project/www/pages/AbstractPage.java
@@ -1,27 +1,31 @@
-package com.project.www.pages;
+package com.agricultura.pages;
 
-import com.project.www.utils.MyFluentWait;
+import com.agricultura.utils.MyFluentWait;
+import io.appium.java_client.TouchAction;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.touch.WaitOptions;
+import io.appium.java_client.touch.offset.PointOption;
 import lombok.extern.slf4j.Slf4j;
 import org.openqa.selenium.*;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Wait;
-
+import java.time.Duration;
 import java.time.temporal.ChronoUnit;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
-//
 @Slf4j
 public abstract class AbstractPage {
+    protected Wait<WebDriver> wait;
+    private final WebDriver driver;
 
-    protected MyFluentWait<WebDriver> wait;
-    protected final WebDriver driver;
-
-    protected AbstractPage(WebDriver driver) {
+    AbstractPage(WebDriver driver) {
         this.driver = driver;
         wait = new MyFluentWait<>(driver)
                 .withTimeout(15, ChronoUnit.SECONDS)
-                .pollingEvery(500, ChronoUnit.MILLIS)
+                .pollingEvery(2, ChronoUnit.SECONDS)
                 .ignoring(NoSuchElementException.class);
     }
 
@@ -35,7 +39,7 @@ public abstract class AbstractPage {
         return wait;
     }
 
-    protected void setWait(MyFluentWait<WebDriver> wait) {
+    protected void setWait(Wait<WebDriver> wait) {
         this.wait = wait;
     }
 
@@ -53,31 +57,87 @@ public abstract class AbstractPage {
         }
     }
 
-    protected boolean isPageLoaded(WebElement elem) {
-        boolean isLoaded = false;
+    protected void click(WebElement element) {
+        wait.until(ExpectedConditions.elementToBeClickable(element));
+        element.click();
+    }
 
+    protected void setText(WebElement element, String text) {
+        wait.until(ExpectedConditions.visibilityOf(element));
+        element.clear();
+        element.sendKeys(text);
+    }
+
+    protected String getText(WebElement element) {
+        wait.until(ExpectedConditions.visibilityOf(element));
+        return element.getText();
+    }
+
+    protected boolean isElementLoaded(WebElement elem) {
         try {
-            isLoaded = elem.isDisplayed();
+            return elem.isDisplayed();
         } catch (NoSuchElementException e) {
             e.printStackTrace();
         }
-        return isLoaded;
+        return false;
     }
 
-    public void navigateTo(String url) {
-        WebDriver driver = getDriver();
+    /**
+     * Obtener elemento por buscqueda mendiante xpath
+     *
+     * @param xPath selector xpath
+     * @return WebElement
+     */
+    protected WebElement getElementByXPath(String xPath) {
+        return getDriver().findElement(By.xpath(xPath));
+    }
 
-        try {
-            driver.navigate().to(url);
-        } catch (Exception e) {
-            if (e instanceof TimeoutException) {
-                log.info("Timeout loading home page");
-            } else if (e instanceof ScriptTimeoutException) {
-                log.info("Script Timeout loading home page");
-            } else {
-                log.error(e.getMessage());
-            }
+    /**
+     * Obtener lista de elementos por buscqueda mendiante xpath
+     *
+     * @param xPath selector xpath
+     * @return List<WebElement>
+     */
+    protected List<WebElement> getElementsByXPath(String xPath) {
+        return getDriver().findElements(By.xpath(xPath));
+    }
+
+    /**
+     * Realizar scroll en pantalla
+     *
+     * @param startX corrdenada x de inicio
+     * @param startY coordenada y de inicio
+     * @param endX   corrdenada x final
+     * @param endY   coordenada y final
+     */
+    protected void scroll(int startX, int startY, int endX, int endY) {
+        getDriver().manage().timeouts().implicitlyWait(200, TimeUnit.MILLISECONDS);
+        TouchAction action = new TouchAction((AndroidDriver) getDriver());
+
+        action.press(PointOption.point(startX, startY))
+                .waitAction(WaitOptions.waitOptions(Duration.ofSeconds(1)))
+                .moveTo(PointOption.point(endX, endY)).release().perform();
+
+    }
+
+    /**
+     * Encontrar elemento via xPath en pantalla mediante scrolls
+     *
+     * @param xPath sentencia xpath
+     * @return WebElement
+     */
+    protected WebElement scrollIntoView(String xPath) {
+        getDriver().manage().timeouts().implicitlyWait(200, TimeUnit.MILLISECONDS);
+        Dimension dimension = getDriver().manage().window().getSize();
+
+        int x = dimension.width / 2;
+        int start_y = dimension.height - 300;
+        int end_y = 300;
+
+        while (getElementsByXPath(xPath).size() == 0) {
+            scroll(x, start_y, x, end_y);
         }
-        log.info(driver.getCurrentUrl());
+
+        return getElementsByXPath(xPath).get(0);
     }
 }
